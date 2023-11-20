d6df06db17a247397fb6e91e9c75df6fb3932bab
dbrick1432 <119401041+dbrick1432@users.noreply.github.com>
2023-11-20 15:33:23 -0800

    Created and Routed the About and Contact Pages

--- routes/index.js
+++ routes/index.js
@@ -13,13 +13,14 @@ router.get('/home', function(req, res, next) {
 
 /* GET home page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('about', { title: 'About' });
 });
 
 /* GET home page. */
 router.get('/list', function(req, res, next) {
   res.render('list', { title: 'List of Surveys' });
 });
+
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
@@ -29,6 +30,11 @@ router.get('/login', function(req, res, next) {
 /* GET home page. */
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register' });
+});
+
+/* GET home page. */
+router.get('/Contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Us' });
 });
 
 module.exports = router;
